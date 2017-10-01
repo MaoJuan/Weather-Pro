f159515e0f138424dbfff7a15bcdd1b214fcdaa7
zhangsan <734790718@qq.com>
2017-10-01 20:42:29 +0800

    first commit

---
+++ app/src/main/java/com/example/mj/myweather/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mj.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MJ on 2017/10/1.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.weather_info);
+    }
+}
