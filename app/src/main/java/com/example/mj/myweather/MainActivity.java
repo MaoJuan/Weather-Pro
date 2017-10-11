4de7b9b83526c35c8284c340f7425ca99ed5e1c6
zhangsan <734790718@qq.com>
2017-10-11 20:05:00 +0800

    first commit

--- app/src/main/java/com/example/mj/myweather/MainActivity.java
+++ app/src/main/java/com/example/mj/myweather/MainActivity.java
@@ -2,15 +2,26 @@ package com.example.mj.myweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import cn.edu.pku.mj.util.NetUtil;
 /**
  * Created by MJ on 2017/10/1.
  */
 
 public class MainActivity extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.weather_info);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("myweather", "网络OK");
+            Toast.makeText(MainActivity.this, "网络ok", Toast.LENGTH_LONG).show();
+        } else {
+            Log.d("myweather", "网络挂了");
+            Toast.makeText(MainActivity.this, "网络挂了", Toast.LENGTH_LONG).show();
+        }
     }
+
 }
