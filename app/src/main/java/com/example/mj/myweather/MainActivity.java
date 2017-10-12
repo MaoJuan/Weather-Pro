6361fb601d482a7bfb8d64ebbcd3f4fae9e20cf9
zhangsan <734790718@qq.com>
2017-10-12 13:24:04 +0800

    first commit

--- app/src/main/java/com/example/mj/myweather/MainActivity.java
+++ app/src/main/java/com/example/mj/myweather/MainActivity.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -40,17 +46,92 @@ public class MainActivity extends Activity implements View.OnClickListener{
         if (view.getId() == R.id.title_update_btn) {
             SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
             String cityCode = sharedPreferences.getString("main_city_code", "101010100");
-            Log.d("myweather",cityCode);
+            Log.d("myWeather",cityCode);
 
             if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
-                Log.d("myweather", "网络OK");
+                Log.d("myWeather", "网络OK");
                 queryWeatherCode(cityCode);
             } else {
-                Log.d("myweather", "网络挂了");
+                Log.d("myWeather", "网络挂了");
                 Toast.makeText(MainActivity.this, "网络挂了！",Toast.LENGTH_LONG).show();
             }
         }
     }
+
+    private void parseXML(String xmldata) {
+        int fengxiangCount = 0;
+        int fengliCount = 0;
+        int dateCount = 0;
+        int highCount = 0;
+        int lowCount = 0;
+        int typeCount = 0;
+        try {
+            XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = fac.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmldata));
+            int eventType = xmlPullParser.getEventType();
+            Log.d("myWeather", "parseXML");
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        if (xmlPullParser.getName().equals("city")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "city:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("updatetime")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "updatetime:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("shidu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "shidu:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("wendu")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "wendu:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("pm25")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "pm25:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("quality")) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "quality:   " + xmlPullParser.getText());
+                        } else if (xmlPullParser.getName().equals("fengxiang") && fengxiangCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "fengxiang:   " + xmlPullParser.getText());
+                            fengxiangCount++;
+                        } else if (xmlPullParser.getName().equals("fengli") && fengliCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "fengli:   " + xmlPullParser.getText());
+                            fengliCount++;
+                        } else if (xmlPullParser.getName().equals("date") && dateCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "date:   " + xmlPullParser.getText());
+                            dateCount++;
+                        } else if (xmlPullParser.getName().equals("high") && highCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "high:   " + xmlPullParser.getText());
+                            highCount++;
+                        } else if (xmlPullParser.getName().equals("low") && lowCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "low:   " + xmlPullParser.getText());
+                            lowCount++;
+                        } else if (xmlPullParser.getName().equals("type") && typeCount == 0) {
+                            eventType = xmlPullParser.next();
+                            Log.d("myWeather", "type:   " + xmlPullParser.getText());
+                            typeCount++;
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                } eventType = xmlPullParser.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
     private void queryWeatherCode(String cityCode){
         final String address="http://wthrcdn.etouch.cn/WeatherApi?citykey="+cityCode;
         Log.d("myWeather",address);
@@ -74,6 +155,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
                     }
                     String responseStr = response.toString();
                     Log.d("myWeather", responseStr);
+                    parseXML(responseStr);
                 } catch(Exception e){
                         e.printStackTrace();
                     }finally{
