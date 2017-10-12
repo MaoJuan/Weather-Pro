c52ec42f2c5b49dd563818acd4c6d607fa0eed4c
zhangsan <734790718@qq.com>
2017-10-12 13:49:32 +0800

    first commit

--- app/src/main/java/com/example/mj/myweather/MainActivity.java
+++ app/src/main/java/com/example/mj/myweather/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -27,6 +28,9 @@ import cn.edu.pku.mj.util.NetUtil;
 
 public class MainActivity extends Activity implements View.OnClickListener{
     private ImageView mUpdateBtn;
+    private TextView cityTv,timeTv,humidityTv,weekTv,pmDataTv,pmQualityTv,temperatureTv,climateTv,windTv,city_name_Tv;
+    private ImageView weatherImg,pmImg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +44,36 @@ public class MainActivity extends Activity implements View.OnClickListener{
             Log.d("myweather", "网络挂了");
             Toast.makeText(MainActivity.this, "网络挂了", Toast.LENGTH_LONG).show();
         }
+        initView();
     }
+        void initView(){
+            city_name_Tv=(TextView)findViewById(R.id.title_city_name);
+            cityTv=(TextView)findViewById(R.id.city);
+            timeTv=(TextView)findViewById(R.id.city);
+            humidityTv=(TextView)findViewById(R.id.humidity);
+            weekTv=(TextView)findViewById(R.id.week_today);
+            pmDataTv=(TextView)findViewById(R.id.pm_data);
+            pmQualityTv=(TextView)findViewById(R.id.pm2_5_quality);
+            pmImg=(ImageView)findViewById(R.id.pm2_5_img);
+            temperatureTv=(TextView)findViewById(R.id.temperature);
+            climateTv=(TextView)findViewById(R.id.climate);
+            windTv=(TextView)findViewById(R.id.wind);
+            weatherImg=(ImageView)findViewById(R.id.weather_img);
+
+            city_name_Tv.setText("N/A");
+            cityTv.setText("N/A");
+            timeTv.setText("N/A");
+            humidityTv.setText("N/A");
+            pmDataTv.setText("N/A");
+            pmQualityTv.setText("N/A");
+            weekTv.setText("N/A");
+            temperatureTv.setText("N/A");
+            climateTv.setText("N/A");
+            windTv.setText("N/A");
+        }
+
+
+
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.title_update_btn) {
