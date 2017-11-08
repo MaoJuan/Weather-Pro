e1dc3c219e374f75217f18582cdf187c926dee2f
zhangsan <734790718@qq.com>
2017-11-08 15:56:48 +0800

    first commit

--- app/src/main/java/com/example/mj/myweather/SelectCity.java
+++ app/src/main/java/com/example/mj/myweather/SelectCity.java
@@ -1,8 +1,10 @@
 package com.example.mj.myweather;
 
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,13 +15,15 @@ import android.widget.ImageView;
 public class SelectCity extends Activity implements View.OnClickListener {
     private ImageView mBackBtn;
 
-    protected void onCreated(Bundle savedInstanceState){
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city);
         mBackBtn=(ImageView)findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
     }
-  public void onClick(View v){
+
+ public void onClick(View v){
         switch(v.getId()){
             case R.id.title_back:
                 Intent i=new Intent();
