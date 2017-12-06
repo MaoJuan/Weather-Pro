53b768427392d4645fce4411dd0245756cd0263f
zhangsan <734790718@qq.com>
2017-12-06 15:41:10 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/mj/bean/City.java
@@ -0,0 +1,73 @@
+package cn.edu.pku.mj.bean;
+
+/**
+ * Created by MJ on 2017/11/14.
+ */
+
+public class City {
+    private String province;
+    private String city;
+    private String number;
+    private String firstPY;
+    private String allPY;
+    private String allFirstPY;
+
+    public City(String province, String city, String number, String firstPY, String allPY, String allFristPY) {
+        this.province=province;
+        this.city=city;
+        this.number=number;
+        this.firstPY=firstPY;
+        this.allPY=allPY;
+        this.allFirstPY=allFristPY;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getFirstPY() {
+        return firstPY;
+    }
+
+    public void setFirstPY(String firstPY) {
+        this.firstPY = firstPY;
+    }
+
+    public String getAllPY() {
+        return allPY;
+    }
+
+    public void setAllPY(String allPY) {
+        this.allPY = allPY;
+    }
+
+    public String getAllFirstPY() {
+        return allFirstPY;
+    }
+
+    public void setAllFirstPY(String allFirstPY) {
+        this.allFirstPY = allFirstPY;
+    }
+
+
+}
